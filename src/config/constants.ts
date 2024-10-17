633f809e73db877db31ba9398f074bade7a8e2ff
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-10-17 13:10:14 +0800

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,1 @@
-export const baseUrl =   'http://198.23.208.13:8000'
-export const reverseClaudeBaseUrl =  'https://ai.liuli.585dg.com' //'http://127.0.0.1:8181'  //  'https://demo.fuclaude.com' //
-// export const officialClaudeBaseUrl = 
-//'https://claude.liuligpt.com'
+export const baseUrl = process.env.BASE_URL || 'http://198.23.208.13:8000';
