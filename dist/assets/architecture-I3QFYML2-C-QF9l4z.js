1e49c3b884442c8640e578dd6f2db686bd617b6d
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2024-11-29 14:52:17 +0800

    feature: add new models

--- dist/assets/architecture-I3QFYML2-C-QF9l4z.js
+++
@@ -1,1 +0,0 @@
-import{A as s,e as u}from"./mermaid-parser.core-BEg_ni5W.js";import"./index-BB1CUsUs.js";import"//at.alicdn.com/t/c/font_4565805_mtkshfnyzag.js";import"./Chat-g7V3mdih.js";import"./SendMessageButton-Ct9mWzRB.js";import"./min-DIIAu_aL.js";import"./reduce-Bxx4ZRR1.js";import"./clone-BkA3t2ur.js";export{s as ArchitectureModule,u as createArchitectureServices};
---
+++ dist/assets/packet-W2GHVCYJ-BEBgdm3p.js
@@ -0,0 +1,1 @@
+import{P as s,a as P}from"./mermaid-parser.core-9wpTKpXR.js";import"./index-DjtH47eF.js";import"//at.alicdn.com/t/c/font_4565805_mtkshfnyzag.js";import"./Chat-BBvU0IGX.js";import"./SendMessageButton-_l7xE-W4.js";import"./min-CiSQkNX6.js";import"./reduce-CAEZ6kDg.js";import"./clone-Dc1QXhqI.js";export{s as PacketModule,P as createPacketServices};
